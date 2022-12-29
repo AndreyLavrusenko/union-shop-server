06d63c09e7d8488135a8a052c9ba5f980cf0189b
andreylavrusenko <m>
2022-12-29 18:17:08 +0300

    change db

--- db.js
+++ db.js
@@ -10,12 +10,17 @@ const union_pool = mysql.createPool({
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    // port: 3306
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+
+    host: process.env.DB_HOST_PROD,
+    user: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    port: 3306
 });
 
 
